aebd7f69bcb0db03bb58e02293cb19807d6f682b
Roman Ishchenko <roman@ishchenko.ck.ua>
2020-03-02 14:14:51 +0200

    Added inline SystemJS import renderer

--- src/systemjs/InlineRenderer.php
+++ src/systemjs/InlineRenderer.php
@@ -9,7 +9,9 @@ namespace ischenko\yii2\jsloader\systemjs;
 
 use ischenko\yii2\jsloader\helpers\JsExpression;
 use ischenko\yii2\jsloader\JsRendererInterface;
+use ischenko\yii2\jsloader\ModuleInterface;
 use yii\base\BaseObject;
+use yii\helpers\Json;
 
 /**
  * JS renderer for SystemJS
@@ -20,11 +22,81 @@ use yii\base\BaseObject;
 class InlineRenderer extends BaseObject implements JsRendererInterface
 {
     /**
+     * @var array
+     */
+    private $seen = [];
+
+    /**
+     * @var int
+     */
+    private $moduleIndex = 1;
+
+    /**
      * @param JsExpression $expression
      * @return string
      */
     public function renderJsExpression(JsExpression $expression): string
     {
+        $dependencies = $expression->getDependencies();
 
+        while (($code = $expression->getExpression()) instanceof JsExpression) {
+            $dependencies = array_merge($dependencies, $code->getDependencies());
+            $expression = $code;
+        }
+
+        $this->seen = $modules = [];
+
+        foreach ($dependencies as $dependency) {
+            $modules = array_merge($this->resolveDependencies($dependency), $modules);
+        }
+
+        $modules = array_filter($modules,
+            function (ModuleInterface $module) {
+                return count($module->getFiles()) == 1;
+            });
+
+        $code = $code ?: '';
+
+        /** @var ModuleInterface $module */
+        foreach (array_reverse($modules) as $module) {
+            $alias = $module->getAlias();
+
+            $import = "System.import(" . Json::encode($alias) . ")";
+            $export = $module->getOptions()['systemjs']['exports'] ?? '';
+
+            if (!empty($export)) {
+                $code = "var {$export} = __sjs_module_" . $this->moduleIndex . ".default;\n{$code}";
+                $code = "function(__sjs_module_" . $this->moduleIndex . ") {\n{$code}\n}";
+
+                $this->moduleIndex++;
+            } else {
+                $code = "function() {\n{$code}\n}";
+            }
+
+            $code = "{$import}.then({$code});";
+        }
+
+        return $code;
+    }
+
+    /**
+     * @param ModuleInterface $module
+     * @return ModuleInterface[]
+     */
+    private function resolveDependencies(ModuleInterface $module): array
+    {
+        $alias = $module->getAlias();
+
+        if (isset($this->seen[$alias])) {
+            return $this->seen[$alias];
+        }
+
+        $dependencies = [$alias => $module];
+
+        foreach ($module->getDependencies() as $dependency) {
+            $dependencies = array_merge($this->resolveDependencies($dependency), $dependencies);
+        }
+
+        return $this->seen[$alias] = $dependencies;
     }
 }
--- tests/unit/InlineRendererTest.php
+++ tests/unit/InlineRendererTest.php
@@ -3,8 +3,101 @@
 namespace ischenko\yii2\jsloader\tests\unit\systemjs;
 
 use Codeception\Test\Unit;
+use ischenko\yii2\jsloader\helpers\JsExpression;
+use ischenko\yii2\jsloader\ModuleInterface;
+use ischenko\yii2\jsloader\systemjs\InlineRenderer;
 
 class InlineRendererTest extends Unit
 {
+    /**
+     * @dataProvider expressionsProvider
+     */
+    public function testExpressionRendering($expression, $expected)
+    {
+        /** @var InlineRenderer $renderer */
+        $renderer = $this->construct(InlineRenderer::class);
 
+        verify($renderer->renderJsExpression($expression))->equals($expected);
+    }
+
+    public function expressionsProvider()
+    {
+        return [
+            [
+                $this->construct(JsExpression::class, []),
+                ""
+            ],
+
+            [
+                $this->construct(JsExpression::class, ['console.log(\'test\');']),
+                "console.log('test');"
+            ],
+
+            [
+                $this->construct(JsExpression::class, [
+                    'console.log(\'test\');',
+                    [
+                        $this->makeEmpty(ModuleInterface::class,
+                            ['getAlias' => 'mod', 'getFiles' => ['testing.js' => []]])
+                    ]
+                ]),
+                "System.import(\"mod\").then(function() {\nconsole.log('test');\n});"
+            ],
+
+            [
+                $this->construct(JsExpression::class, [
+                    'console.log(\'test\');',
+                    [
+                        $this->makeEmpty(ModuleInterface::class, [
+                            'getAlias' => 'mod',
+                            'getOptions' => [
+                                'systemjs' => ['exports' => 'export_alias']
+                            ],
+                            'getFiles' => ['testing.js' => []]
+                        ])
+                    ]
+                ]),
+                "System.import(\"mod\").then(function(__sjs_module_1) {\nvar export_alias = __sjs_module_1.default;\nconsole.log('test');\n});"
+            ],
+
+            [
+                $this->construct(JsExpression::class, [
+                    'console.log(\'test\');',
+                    [
+                        $this->makeEmpty(ModuleInterface::class, [
+                            'getAlias' => 'mod1',
+                            'getOptions' => [
+                                'systemjs' => ['exports' => 'export_alias']
+                            ],
+                            'getFiles' => ['testing.js' => []]
+                        ]),
+                        $this->makeEmpty(ModuleInterface::class, [
+                            'getAlias' => 'mod2'
+                        ])
+                    ]
+                ]),
+                "System.import(\"mod1\").then(function(__sjs_module_1) {\nvar export_alias = __sjs_module_1.default;\nconsole.log('test');\n});"
+            ],
+
+            [
+                $this->construct(JsExpression::class, [
+                    'console.log(\'test\');',
+                    [
+                        $this->makeEmpty(ModuleInterface::class, [
+                            'getAlias' => 'mod1',
+                            'getOptions' => [
+                                'systemjs' => ['exports' => 'export_alias']
+                            ],
+                            'getFiles' => ['testing.js' => []]
+                        ]),
+                        $this->makeEmpty(ModuleInterface::class, [
+                            'getAlias' => 'mod2',
+                            'getFiles' => ['testing2.js' => []]
+                        ])
+                    ]
+                ]),
+                "System.import(\"mod2\").then(function() {\nSystem.import(\"mod1\").then(function(__sjs_module_1) {\nvar export_alias = __sjs_module_1.default;\nconsole.log('test');\n});\n});"
+            ],
+        ];
+    }
 }
